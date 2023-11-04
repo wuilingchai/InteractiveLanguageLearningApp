64bf32ea29191d965da7a74c54783991cea4d61f
Han <yuhan1791@gmail.com>
2023-11-04 19:51:09 +0800

    Fixed bug

--- base/forms.py
+++ base/forms.py
@@ -1,19 +1,22 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
-from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# These no need, because you use your own User Model, if you import this from the bottom you will use the django default one
+# from django.contrib.auth.models import User
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
 
 class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ('name', 'avatar')
+        fields = UserCreationForm.Meta.fields + ('avatar', 'name')
         # ['avatar', 'name', 'username', 'email', 'password1', 'password2']
 
 class UserForm(ModelForm):
     class Meta:
         model = User
-        field = ['username', 'email']
+        # typo also is fields not field
+        fields = ['username', 'email']
         # fields = ['avatar', 'name', 'username', 'email']
