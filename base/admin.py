59b51c5da7f8ed62c2ac9a3696e9c122937afa2b
Han <yuhan1791@gmail.com>
2023-11-04 21:03:17 +0800

    update

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
 # from slangdictionary.models import Slang
 # from learningmodule.models import Vocabulary
 
-# admin.site.register(Slang)
+admin.site.register(User)
 # admin.site.register(Vocabulary)
